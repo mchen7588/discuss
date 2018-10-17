30c880407e901f683a0861158507d3d91b943fd4
Michael Chen <mchen7588@gmail.com>
2018-10-17 13:25:21 -0700

    save new user info in db

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,12 +2,32 @@ defmodule Discuss.AuthController do
     use Discuss.Web, :controller
     plug Ueberauth
   
-    def callback(conn, params) do
-      IO.puts "+++++"
-      IO.inspect(conn.assigns)
-      IO.puts "+++++"
-      IO.inspect(params)
-      IO.puts "+++++"
+    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider} = params) do
+        IO.puts "+++++"
+        IO.inspect(conn.assigns)
+        IO.puts "+++++"
+        IO.inspect(params)
+        IO.puts "+++++"
+
+        user_params = %{token: auth.credentials.token, email: auth.info.email, provider: provider}
+
+        changeset = Discuss.User.changeset(%Discuss.User{}, user_params)
+
+        IO.puts("**********")
+        IO.inspect(changeset)
+
+        insert_or_update_user(changeset)
+    end
+
+    defp insert_or_update_user(%Ecto.Changeset{changes: %{email: email}} = changeset) do
+        IO.puts("###########")
+        IO.inspect(email)
+
+        case Repo.get_by(Discuss.User, email: email) do
+            user ->
+                {:ok, user}
+            nil ->
+                Repo.insert(changeset)
+        end
     end
 end
-  
