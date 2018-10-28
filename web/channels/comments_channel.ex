649291a1915537004aae1a66148224320bbbe3af
Michael Chen <mchen7588@gmail.com>
2018-10-28 09:59:19 -0700

    connect to comment socket

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,8 +1,11 @@
 defmodule Discuss.CommentsChannel do
     use Discuss.Web, :channel
 
-    def join() do
-        
+    def join(name, _params, socket) do  # this is called when a javascript client connect to this channel
+        IO.puts("*************")
+        IO.puts(name)
+
+        {:ok, %{what: "what"}, socket}
     end
 
     def handle_in() do
