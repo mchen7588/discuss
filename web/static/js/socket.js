bd6c9ad2c3e9a6f665de8d97e3cfa34d63f354a8
Michael Chen <mchen7588@gmail.com>
2018-10-31 09:21:51 -0700

    join comments socket channel with specific topic id

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -4,9 +4,12 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-let channel = socket.channel("comments:1", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
 
-export default socket
+const createSocket = (topicId) => {
+  let channel = socket.channel(`comments:${topicId}`, {})
+  channel.join()
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+}
+
+window.createSocket = createSocket;
