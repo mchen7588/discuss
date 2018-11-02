4004fb08b282041cb5b4e9945be7e0720d18822d
Michael Chen <mchen7588@gmail.com>
2018-11-01 18:30:10 -0700

    save comment in db on submitting comment

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -3,19 +3,32 @@ defmodule Discuss.CommentsChannel do
 
     def join("comments:" <> topic_id = name, _params, socket) do  # this is called when a javascript client connect to this channel
         topic_id = String.to_integer(topic_id)
-        Repo.get(Discuss.Topic, topic_id)
+        topic = Repo.get(Discuss.Topic, topic_id)
 
         IO.puts("*************")
         IO.puts(name)
 
-        {:ok, %{what: "what"}, socket}
+        {:ok, %{what: "what"}, assign(socket, :topic, topic)}
     end
 
-    def handle_in(name, message, socket) do  # this is called when a javascript client broadcast an event to this channel
+    def handle_in(name, %{"content" => content} = message, %{assigns: %{topic: topic}} = socket) do  # this is called when a javascript client broadcast an event to this channel
         IO.puts("#############")
         IO.inspect(message)
+        IO.puts("$$$$$$$$$$$$$")
+        IO.inspect(topic)
 
-        {:reply, :ok, socket}
+        changeset = topic
+            |> build_assoc(:comment)
+            |> Discuss.Comment.changeset(%{content: content})
+
+        case Repo.insert(changeset) do
+            {:ok, _} ->
+                {:reply, :ok, socket}
+            {:error, _} ->
+                {:reply, {:error, %{error: changeset}}, socket}
+
+        end
+
     end
     
 end
