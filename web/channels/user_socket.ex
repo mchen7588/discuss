be974e44ddf11742674613675ca1279a9e3792f9
Michael Chen <mchen7588@gmail.com>
2018-11-06 09:48:19 -0800

    generate user token and add on window

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,9 @@ defmodule Discuss.UserSocket do
 
   transport :websocket, Phoenix.Transports.WebSocket
 
-  def connect(_params, socket) do
+  def connect(%{"token" => token}, socket) do
+    IO.puts("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+    IO.puts token
     {:ok, socket}
   end
 
