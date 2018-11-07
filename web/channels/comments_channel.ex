f1d3e5a7f93ea7d3e6898a601488aa2e89b2d22b
Michael Chen <mchen7588@gmail.com>
2018-11-07 07:41:21 -0800

    associate comments with user

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -2,7 +2,7 @@ defmodule Discuss.CommentsChannel do
     use Discuss.Web, :channel
     alias Discuss.{ Topic, Comment }
 
-    def join("comments:" <> topic_id = name, _params, socket) do  # this is called when a javascript client connect to this channel
+    def join("comments:" <> topic_id, _params, socket) do  # this is called when a javascript client connect to this channel
         topic_id = String.to_integer(topic_id)
         topic = Topic
             |> Repo.get(topic_id)
@@ -11,9 +11,9 @@ defmodule Discuss.CommentsChannel do
         {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
     end
 
-    def handle_in(name, %{"content" => content}, %{assigns: %{topic: topic}} = socket) do  # this is called when a javascript client broadcast an event to this channel
+    def handle_in(_name, %{"content" => content}, %{assigns: %{topic: topic, user_id: user_id}} = socket) do  # this is called when a javascript client broadcast an event to this channel
         changeset = topic
-            |> build_assoc(:comments)
+            |> build_assoc(:comments, user_id: user_id)
             |> Comment.changeset(%{content: content})
 
         case Repo.insert(changeset) do
